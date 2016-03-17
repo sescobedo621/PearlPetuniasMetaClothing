288547a7fd144f39f55030de05bf04143a39dd69
Shelby Escobedo <shelby.escobedo@gmail.com>
2016-03-17 10:53:20 -0600

    many to one relationship for user

--- MetaClothes/MetaClothingJPA/src/entity/Address.java
+++ MetaClothes/MetaClothingJPA/src/entity/Address.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -20,6 +22,8 @@ public class Address {
 	private String stateAbbrev;
 	@Column(name="zip")
 	private int zipcode;
+	@ManyToOne
+	@JoinColumn(name="userid")
 	private User user;
 	@Column(name="isbilling")
 	private boolean isBilling;
